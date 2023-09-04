0e7e8c56b2501bf212b53d3e62db1c789072243d
pedromessetti <pedromessetti@gmail.com>
2023-09-04 19:37:28 +0100

    check if the file exists, create and tag

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"os"
+
 	gen "github.com/pedromessetti/tag_file/gen"
+	tag "github.com/pedromessetti/tag_file/tag"
+
 )
 
 func main() {
-	gen.GenerateRandomUsers(100)
+	file := "users.txt"
+
+	// Check if the file exists, if doesn't, generate it with random users
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		gen.GenerateRandomUsers(10000)
+	}
+
+	tag.TagFile(file)
 }
